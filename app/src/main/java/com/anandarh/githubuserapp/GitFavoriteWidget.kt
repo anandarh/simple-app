7bdf34de1f4cdce8a89489ec0751d5f2ba7b1856
Risa Herdianto <aputra.zha@gmail.com>
2021-06-21 23:09:10 +0700

    Added android widget feature

---
+++ app/src/main/java/com/anandarh/githubuserapp/GitFavoriteWidget.kt
@@ -0,0 +1,81 @@
+package com.anandarh.githubuserapp
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+import android.widget.Toast
+import androidx.core.net.toUri
+import com.anandarh.githubuserapp.services.StackWidgetService
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class GitFavoriteWidget : AppWidgetProvider() {
+
+    companion object {
+
+        private const val TOAST_ACTION = "com.anandarh.githubuserapp.TOAST_ACTION"
+        const val EXTRA_ITEM = "com.anandarh.githubuserapp.EXTRA_ITEM"
+
+        private fun updateAppWidget(
+            context: Context,
+            appWidgetManager: AppWidgetManager,
+            appWidgetId: Int
+        ) {
+            val intent = Intent(context, StackWidgetService::class.java)
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+            intent.data = intent.toUri(Intent.URI_INTENT_SCHEME).toUri()
+
+            val views = RemoteViews(context.packageName, R.layout.git_favorite_widget)
+            views.setRemoteAdapter(R.id.stackView, intent)
+            views.setEmptyView(R.id.stackView, R.id.empty_view)
+
+            val toastIntent = Intent(context, GitFavoriteWidget::class.java)
+            toastIntent.action = TOAST_ACTION
+            toastIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+            toastIntent.data = toastIntent.toUri(Intent.URI_INTENT_SCHEME).toUri()
+
+            val toastPendingIntent = PendingIntent.getBroadcast(
+                context,
+                0,
+                toastIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+            )
+            views.setPendingIntentTemplate(R.id.stackView, toastPendingIntent)
+
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        super.onReceive(context, intent)
+        if (intent?.action != null) {
+            if (intent.action == TOAST_ACTION) {
+                val viewIndex = intent.getStringExtra(EXTRA_ITEM)
+                Toast.makeText(context, "$viewIndex", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
